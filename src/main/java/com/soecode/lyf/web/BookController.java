ddc0453478855592f92ca26b9b3f7a127a65fd26
weihan <ica10888@gmail.com>
2017-09-04 16:19:26 +0800

    Update BookController.java
    修改了未导入RequestParam包的错误。

--- src/main/java/com/soecode/lyf/web/BookController.java
+++ src/main/java/com/soecode/lyf/web/BookController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.soecode.lyf.dto.AppointExecution;
 import com.soecode.lyf.dto.Result;
